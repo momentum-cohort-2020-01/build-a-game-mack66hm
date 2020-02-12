8c1eeb2ebddcd24a2257149840f6f3150cc1df4e
mack66hm <mack66hm@gmail.com>
2020-02-12 18:26:12 -0500

    get stuff to move and show up

--- game.js
+++ game.js
@@ -12,42 +12,38 @@ class Game {
     const tick = () => {
       this.update()
       this.draw(context, gameSize)
-    //   requestAnimationFrame(tick)
+      requestAnimationFrame(tick)
     }
-    // tick()
+    tick()
+  }
+
+  update () {
+    // const notHittingAnything = (b1) => {
+    //   return this.bodies.filter(function (b2) { return hitting(b1, b2) }).length === 0
+    // }
+    // this.bodies = this.bodies.filter(notHittingAnything)
+
+    for (let i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].update()
+    }
   }
 
   draw (context, gameSize) {
-    // context.clearRect(0, 0, gameSize.x, gameSize.y)
+    context.clearRect(0, 0, gameSize.x, gameSize.y)
 
     for (let i = 0; i < this.bodies.length; i++) {
-      drawRect(context, this.bodies[1])
+      drawRect(context, this.bodies[i])
     }
   }
-}
 
-// draw (context, gameSize) {
-//     context.clearRect(0, 0, gameSize.x, gameSize.y)
+//   enemyBelow (enemy) {
+//     return this.bodies.filter(function (b) {
+//       return b instanceof Enemy && Math.abs(enemy.center.x - b.center.x) < b.size.x && b.center.y > enemy.center.y
+//     }).length > 0
+//   }
 
-//     for (let i = 0; i < this.bodies.length; i++) {
-//         drawRect(context, this.bodies[1])
-//     }
-// }
-
-class Player {
-  constructor (game, gameSize) {
-    this.game = game
-    this.size = { x: 15, y: 15 }
-    // this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
-    // this.keyboarder = new Keyboarder()
-  }
-
-//   update () {
-//     if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
-//       this.center.x -= 2
-//     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
-//       this.center.x += 2
-//     }
+//   addBody (body) {
+//     this.bodies.push(body)
 //   }
 }
 
@@ -56,6 +52,17 @@ class Enemy {
     this.game = game
     this.center = center
     this.size = { x: 15, y: 15 }
+
+    this.patrolX = 0
+    this.speedX = 0.3
+  }
+
+  update () {
+//     if (this.patrolX < 0 || this.patrolX > 30) {
+//       this.speedX = -this.speedX
+//     }
+//     this.center.x += this.speedX
+//     this.patrolX += this.speedX
   }
 }
 
@@ -69,27 +76,41 @@ function createEnemy (game) {
   return enemy
 }
 
-// class Drawing {
-//   constructor (canvasId) {
-//     this.canvas = document.getElementById(canvasId)
-//     this.context = this.canvas.getContext('2d')
-//     this.points = []
-//   }
-// }
+class Player {
+  constructor (game, gameSize) {
+    this.game = game
+    this.size = { x: 15, y: 15 }
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
+    this.keyboarder = Keyboarder
+  }
+
+  update () {
+    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+      this.center.x -= 2
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+      this.center.x += 2
+    }
+  }
+}
 
 function drawRect (context, body) {
   context.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y)
 }
-// class Enemy {
-//   constructor () {
-//     this.position = { x: 0, y: 0 }
-//   }
+
+// function hitting (b1, b2) {
+//   return !(
+//     b1 === b2 ||
+//             b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+//             b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+//             b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+//             b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+//   )
 // }
 
-// window.addEventListener('load', function () {
-//   new Game()
-// })
+window.addEventListener('load', function () {
+  new Game()
+})
 
-new Game()
-new Player()
-new Enemy()
+
+// new Player()
+// new Enemy()
