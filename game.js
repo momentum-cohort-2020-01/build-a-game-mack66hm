a45872048c6598f79fd11a7cf6d5abce420feb8a
mack66hm <mack66hm@gmail.com>
2020-02-13 14:29:30 -0500

    update using more keys and backgrounds

--- game.js
+++ game.js
@@ -30,6 +30,7 @@ class Game {
 
   draw (context, gameSize) {
     context.clearRect(0, 0, gameSize.x, gameSize.y)
+    context.fillStyle = '#03fccf';
 
     for (let i = 0; i < this.bodies.length; i++) {
       drawRect(context, this.bodies[i])
@@ -51,26 +52,30 @@ class Enemy {
   constructor (game, center) {
     this.game = game
     this.center = center
-    this.size = { x: 15, y: 15 }
+    this.size = { x: 25, y: 25 }
 
     this.patrolX = 0
-    this.speedX = 0.3
+    this.speedX = 0.5
   }
 
   update () {
-    if (this.patrolX < 0 || this.patrolX > 30) {
+    if (this.patrolX < 0 || this.patrolX > 50) {
       this.speedX = -this.speedX
     }
+    // if (this.patrolY < 0 || this.patrolY > 50) {
+    //     this.speedY = -this.speedY
+    // }
     this.center.x += this.speedX
     this.patrolX += this.speedX
+   
   }
 }
 
 function createEnemy (game) {
   const enemy = []
   for (let i = 0; i < 10; i++) {
-    const x = 30 + (i % 8) * 30
-    const y = 30 + (i % 3) * 30
+    const x = 50 + (i % 8) * 50
+    const y = 50 + (i % 3) * 50
     enemy.push(new Enemy(game, { x: x, y: y }))
   }
   return enemy
@@ -79,7 +84,8 @@ function createEnemy (game) {
 class Player {
   constructor (game, gameSize) {
     this.game = game
-    this.size = { x: 15, y: 15 }
+    this.size = { x: 25, y: 25 } 
+    this.death = false;
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
     this.keyboarder = Keyboarder
   }
@@ -89,12 +95,16 @@ class Player {
       this.center.x -= 2
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
       this.center.x += 2
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
+      this.center.y += 2
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN)) {
+      this.center.y -= 2
     }
   }
 }
 
 function drawRect (context, body) {
-  context.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y)
+    context.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y)
 }
 
 function hitting (b1, b2) {
@@ -110,4 +120,3 @@ function hitting (b1, b2) {
 window.addEventListener('load', function () {
   new Game()
 })
-
